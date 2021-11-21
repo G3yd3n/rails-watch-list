b3afc1fe34645666e2eca04ffe9e6c254bf2e487
soonteckling <soonteckl@outlook.com>
2021-11-21 22:48:26 +0800

    beautify front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'pry-byebug'
 
-puts "Cleaning database..."
-# Movie.destory_all
 puts "creating database..."
-Bookmark.create()
+
+data = JSON.parse(URI.open("http://tmdb.lewagon.com/movie/top_rated").read)
+results = data["results"].take(10)
+
+results.each do |result|
+  movie = Movie.new(
+    title: result["title"],
+    overview: result["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{result["backdrop_path"]}",
+    rating: result["vote_average"]
+  )
+  movie.save
+  puts "creating #{movie.id} with #{movie.title}"
+end
+
+puts "finished"
+
+
+
+  # create_table "movies", force: :cascade do |t|
+  #   t.string "title"
+  #   t.text "overview"
+  #   t.string "poster_url"
+  #   t.integer "rating"
